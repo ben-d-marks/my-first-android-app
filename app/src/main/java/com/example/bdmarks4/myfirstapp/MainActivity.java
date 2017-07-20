0baf6d36c6f3d6602616d7af63cd66e70a4aa729
Belve D. Marks <belvedmarks4@gmail.com>
2017-07-20 13:32:09 -0500

    init project -- this is just a bootstrap

---
+++ app/src/main/java/com/example/bdmarks4/myfirstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bdmarks4.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
